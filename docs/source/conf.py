457ba99cd1fc51faacf1c5ef75a3410913a511c5
PHR <98035958+Pejman712@users.noreply.github.com>
2024-01-16 15:18:36 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Dbot_ROS_Humble'
+copyright = '2024, Pejman'
+author = 'Pejman Habibiroudkenar'
 
 release = '0.1'
 version = '0.1.0'
